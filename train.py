5a32839434c9c4ab2d9a4d289a0813e76c5dfbdb
Victor Zhao <victorzhao1999@gmail.com>
2022-04-14 19:11:07 +0100

    Implement GCPN, GraphAF, and GIN

--- train.py
+++ train.py
@@ -2,8 +2,10 @@ import argparse
 import os
 import pickle
 
-from torch import nn, optim
+import torch
+from torch import optim
 from torchdrug import datasets, models, tasks, core
+from torchdrug.layers import distribution
 
 
 def load_dataset(dataset_dir):
@@ -18,27 +20,147 @@ def load_dataset(dataset_dir):
     return dataset
 
 
-def pretrain_GCPN(dataset, num_epochs, checkpoint_dir):
-    model = models.RGCN(input_dim=dataset.node_feature_dim, hidden_dims=[256, 256, 256, 256],
-                        num_relation=dataset.num_bond_type, batch_norm=False)
-    task = tasks.GCPNGeneration(model, dataset.atom_types, max_edge_unroll=12, max_node=38, criterion='nll')
+def train_GCPN(dataset, gnn_type, checkpoint_dir,
+               num_pretrain_epochs=1,
+               num_finetune_epochs=10):
+    if gnn_type == 'RGCN':
+        model = models.RGCN(input_dim=dataset.node_feature_dim,
+                            hidden_dims=[256, 256, 256, 256],
+                            num_relation=dataset.num_bond_type,
+                            batch_norm=False)
+    elif gnn_type == 'GIN':
+        model = models.GIN(input_dim=dataset.node_feature_dim,
+                           hidden_dims=[256, 256, 256, 256],
+                           batch_norm=False)
+    else:
+        raise NotImplementedError
 
-    optimizer = optim.Adam(task.parameters(), lr=1e-3)
-    solver = core.Engine(task, dataset, None, None, optimizer, gpus=(0,), batch_size=128, log_interval=10)
+    # Pre-training
+    pretrain_task = tasks.GCPNGeneration(model, dataset.atom_types,
+                                         max_edge_unroll=12, max_node=38,
+                                         criterion='nll')
+    pretrain_optimizer = optim.Adam(pretrain_task.parameters(), lr=1e-3)
+    pretrain_solver = core.Engine(pretrain_task, dataset, None, None,
+                                  pretrain_optimizer, gpus=(0,),
+                                  batch_size=128, log_interval=10)
 
-    solver.train(num_epoch=1)
-    solver.save(os.path.join(checkpoint_dir, f'gcpn_zinc250k_{num_epochs}epochs.pickle'))
+    pretrain_solver.train(num_epoch=num_pretrain_epochs)
+    if num_pretrain_epochs > 1:
+        pretrain_model_name = f'gcpn_zinc250k_{num_pretrain_epochs}epochs.pickle'
+    else:
+        pretrain_model_name = f'gcpn_zinc250k_{num_pretrain_epochs}epoch.pickle'
+    pretrain_solver.save(os.path.join(checkpoint_dir, pretrain_model_name))
 
-    return task, optimizer, solver
+    pretrain_results = pretrain_task.generate(num_sample=32, max_resample=5)
+    print(pretrain_results.to_smiles())
+
+    # Fine-tuning
+    finetune_task = tasks.GCPNGeneration(model, dataset.atom_types,
+                                         max_edge_unroll=12, max_node=38,
+                                         task="plogp", criterion="ppo",
+                                         reward_temperature=1,
+                                         agent_update_interval=3,
+                                         gamma=0.9)
+    finetune_optimizer = optim.Adam(finetune_task.parameters(), lr=1e-5)
+    finetune_solver = core.Engine(finetune_task, dataset, None, None,
+                                  finetune_optimizer, gpus=(0,),
+                                  batch_size=16, log_interval=10)
+    finetune_solver.load(os.path.join(checkpoint_dir, pretrain_model_name),
+                         load_optimizer=False)
+
+    finetune_solver.train(num_epoch=num_finetune_epochs)
+    if num_pretrain_epochs > 1:
+        finetune_model_name = f'gcpn_zinc250k_{num_pretrain_epochs}epochs_finetune.pickle'
+    else:
+        finetune_model_name = f'gcpn_zinc250k_{num_pretrain_epochs}epoch_finetune.pickle'
+    finetune_solver.save(os.path.join(checkpoint_dir, finetune_model_name))
+
+    finetune_results = finetune_task.generate(num_sample=32, max_resample=5)
+    print(finetune_results.to_smiles())
+
+
+def train_GraphAF(dataset, gnn_type, checkpoint_dir,
+                  num_pretrain_epochs=10,
+                  num_finetune_epochs=10):
+    if gnn_type == 'RGCN':
+        model = models.RGCN(input_dim=dataset.node_feature_dim,
+                            hidden_dims=[256, 256, 256],
+                            num_relation=dataset.num_bond_type,
+                            batch_norm=True)
+    elif gnn_type == 'GIN':
+        model = models.GIN(input_dim=dataset.node_feature_dim,
+                           hidden_dims=[256, 256, 256],
+                           batch_norm=True)
+    else:
+        raise NotImplementedError
+
+    num_atom_type = dataset.num_atom_type
+    num_bond_type = dataset.num_bond_type + 1  # add one class for non-edge
+
+    node_prior = distribution.IndependentGaussian(torch.zeros(num_atom_type),
+                                                  torch.ones(num_atom_type))
+    edge_prior = distribution.IndependentGaussian(torch.zeros(num_bond_type),
+                                                  torch.ones(num_bond_type))
+    node_flow = models.GraphAF(model, node_prior, num_layer=12)
+    edge_flow = models.GraphAF(model, edge_prior, use_edge=True, num_layer=12)
+
+    # Pre-training
+    pretrain_task = tasks.AutoregressiveGeneration(node_flow, edge_flow,
+                                                   max_edge_unroll=12, max_node=38,
+                                                   criterion="nll")
+    pretrain_optimizer = optim.Adam(pretrain_task.parameters(), lr=1e-3)
+    pretrain_solver = core.Engine(pretrain_task, dataset, None, None,
+                                  pretrain_optimizer, gpus=(0,),
+                                  batch_size=128, log_interval=10)
+
+    pretrain_solver.train(num_epoch=num_pretrain_epochs)
+    if num_pretrain_epochs > 1:
+        pretrain_model_name = f'graphaf_zinc250k_{num_pretrain_epochs}epochs.pickle'
+    else:
+        pretrain_model_name = f'graphaf_zinc250k_{num_pretrain_epochs}epoch.pickle'
+    pretrain_solver.save(os.path.join(checkpoint_dir, pretrain_model_name))
+
+    pretrain_results = pretrain_task.generate(num_sample=32)
+    print(pretrain_results.to_smiles())
+
+    # Fine-tuning
+    finetune_task = tasks.AutoregressiveGeneration(node_flow, edge_flow,
+                                                   max_edge_unroll=12, max_node=38,
+                                                   task="plogp", criterion="ppo",
+                                                   reward_temperature=20,
+                                                   baseline_momentum=0.9,
+                                                   agent_update_interval=5,
+                                                   gamma=0.9)
+    finetune_optimizer = optim.Adam(finetune_task.parameters(), lr=1e-5)
+    finetune_solver = core.Engine(finetune_task, dataset, None, None,
+                                  finetune_optimizer, gpus=(0,),
+                                  batch_size=64, log_interval=10)
+    finetune_solver.load(os.path.join(checkpoint_dir, pretrain_model_name),
+                         load_optimizer=False)
+
+    finetune_solver.train(num_epoch=num_finetune_epochs)
+    if num_pretrain_epochs > 1:
+        finetune_model_name = f'graphaf_zinc250k_{num_pretrain_epochs}epochs_finetune.pickle'
+    else:
+        finetune_model_name = f'graphaf_zinc250k_{num_pretrain_epochs}epoch_finetune.pickle'
+    finetune_solver.save(os.path.join(checkpoint_dir, finetune_model_name))
+
+    finetune_results = finetune_task.generate(num_sample=32)
+    print(finetune_results.to_smiles())
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--model_type', type=str, required=True)
+    parser.add_argument('--gnn_type', type=str, required=True)
     parser.add_argument('--data_dir', type=str, required=True)
     parser.add_argument('--checkpoint_dir', type=str, required=True)
     args = parser.parse_args()
 
     dataset = load_dataset(args.data_dir)
-    task, optimizer, solver = pretrain_GCPN(dataset, num_epochs=1, checkpoint_dir=args.checkpoint_dir)
-    results = task.generate(num_sample=32, max_resample=5)
-    print(results.to_smiles())
+    if args.model_type == 'GCPN':
+        train_GCPN(dataset=dataset, gnn_type=args.gnn_type,
+                   checkpoint_dir=args.checkpoint_dir)
+    elif args.model_type == 'GraphAF':
+        train_GraphAF(dataset=dataset, gnn_type=args.gnn_type,
+                      checkpoint_dir=args.checkpoint_dir)
