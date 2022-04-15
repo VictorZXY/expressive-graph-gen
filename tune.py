7299205bd827a106ec952768bf54644d12aadbfa
Victor Zhao <victorzhao1999@gmail.com>
2022-04-15 17:57:48 +0100

    Implement QED task

--- tune.py
+++ tune.py
@@ -8,15 +8,26 @@ from torchdrug.layers import distribution
 from utils import load_dataset, load_GNN
 
 
-def finetune_GCPN(dataset, gnn_type, pretrained_model_path, num_epoch=10):
+def finetune_GCPN(dataset, gnn_type, task, pretrained_model_path, num_epoch=10):
     model = load_GNN(dataset=dataset, model_type='GCPN', gnn_type=gnn_type)
 
-    finetune_task = tasks.GCPNGeneration(model, dataset.atom_types,
-                                         max_edge_unroll=12, max_node=38,
-                                         task="plogp", criterion="ppo",
-                                         reward_temperature=1,
-                                         agent_update_interval=3,
-                                         gamma=0.9)
+    if task == 'plogp':
+        finetune_task = tasks.GCPNGeneration(model, dataset.atom_types,
+                                             max_edge_unroll=12, max_node=38,
+                                             task="plogp", criterion="ppo",
+                                             reward_temperature=1,
+                                             agent_update_interval=3,
+                                             gamma=0.9)
+    elif task == 'qed':
+        finetune_task = tasks.GCPNGeneration(model, dataset.atom_types,
+                                             max_edge_unroll=12, max_node=38,
+                                             task="qed", criterion=("ppo", "nll"),
+                                             reward_temperature=1,
+                                             agent_update_interval=3,
+                                             gamma=0.9)
+    else:
+        assert False
+
     finetune_optimizer = optim.Adam(finetune_task.parameters(), lr=2.5e-4)
     finetune_solver = core.Engine(finetune_task, dataset, None, None,
                                   finetune_optimizer, gpus=(0,),
@@ -31,7 +42,7 @@ def finetune_GCPN(dataset, gnn_type, pretrained_model_path, num_epoch=10):
     print(finetune_results.to_smiles())
 
 
-def finetune_GraphAF(dataset, gnn_type, pretrained_model_path, num_epoch=10):
+def finetune_GraphAF(dataset, gnn_type, task, pretrained_model_path, num_epoch=10):
     model = load_GNN(dataset=dataset, model_type='GraphAF', gnn_type=gnn_type)
 
     num_atom_type = dataset.num_atom_type
@@ -44,13 +55,25 @@ def finetune_GraphAF(dataset, gnn_type, pretrained_model_path, num_epoch=10):
     node_flow = models.GraphAF(model, node_prior, num_layer=12)
     edge_flow = models.GraphAF(model, edge_prior, use_edge=True, num_layer=12)
 
-    finetune_task = tasks.AutoregressiveGeneration(node_flow, edge_flow,
-                                                   max_edge_unroll=12, max_node=38,
-                                                   task="plogp", criterion="ppo",
-                                                   reward_temperature=20,
-                                                   baseline_momentum=0.9,
-                                                   agent_update_interval=5,
-                                                   gamma=0.9)
+    if task == 'plogp':
+        finetune_task = tasks.AutoregressiveGeneration(node_flow, edge_flow,
+                                                       max_edge_unroll=12, max_node=38,
+                                                       task="plogp", criterion="ppo",
+                                                       reward_temperature=20,
+                                                       baseline_momentum=0.9,
+                                                       agent_update_interval=5,
+                                                       gamma=0.9)
+    elif task == 'qed':
+        finetune_task = tasks.AutoregressiveGeneration(node_flow, edge_flow,
+                                                       max_node=38, max_edge_unroll=12,
+                                                       task="qed", criterion={"ppo": 0.25, "nll": 1.0},
+                                                       reward_temperature=10,
+                                                       baseline_momentum=0.9,
+                                                       agent_update_interval=5,
+                                                       gamma=0.9)
+    else:
+        assert False
+
     finetune_optimizer = optim.Adam(finetune_task.parameters(), lr=1e-4)
     finetune_solver = core.Engine(finetune_task, dataset, None, None,
                                   finetune_optimizer, gpus=(0,),
@@ -69,14 +92,15 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_type', type=str, required=True)
     parser.add_argument('--gnn_type', type=str, required=True)
+    parser.add_argument('--task', type=str, required=True)
     parser.add_argument('--data_dir', type=str, required=True)
     parser.add_argument('--pretrained_model_path', type=str, required=True)
     args = parser.parse_args()
 
     dataset = load_dataset(args.data_dir)
     if args.model_type == 'GCPN':
-        finetune_GCPN(dataset=dataset, gnn_type=args.gnn_type,
+        finetune_GCPN(dataset=dataset, gnn_type=args.gnn_type, task=args.task,
                       pretrained_model_path=args.pretrained_model_path)
     elif args.model_type == 'GraphAF':
-        finetune_GraphAF(dataset=dataset, gnn_type=args.gnn_type,
+        finetune_GraphAF(dataset=dataset, gnn_type=args.gnn_type, task=args.task,
                          pretrained_model_path=args.pretrained_model_path)
